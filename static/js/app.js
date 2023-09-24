0044be16eacbcf95a14c41581b692304bcb53d1d
mmyrahim <mmyrahim@outlook.com>
2023-09-24 18:36:24 +0100

    adding code error, waitng for bcs

--- static/js/app.js
+++ static/js/app.js
@@ -88,4 +88,65 @@ function barChart(sample) {
 
 // retriveing json data
 d3.json(url).then((data) => {
+        
 
+// set variables for bar chart and log
+let dataSamples = data.samples;
+let resultID = dataSamples.filter(result => result.id == sample);
+let firstID = resultID[0];
+let otu_ids = firstID.otu_ids;
+let otu_labels = firstID.otu_labels;
+let sample_values = firstID.sample_values;
+
+console.log(otu_ids,otu_labels,sample_values);
+
+
+
+// set up variables to graph (show in descending order)
+let y_axis = otu_ids.slice(0,10).map(id => `OTU ${id}`).reverse();
+let x_axis = sample_values.slice(0,10).reverse();
+let labels = otu_labels.slice(0,10).reverse();
+
+
+
+// create trace for bar chart
+let colors = ['#FF33A1', '#FF5733', '#33FF57','#33FFA1', '#33A1FF', '#A1FF33', '#A133FF', '#A1FFA1', '#5733FF', '#FFA133',] 
+let traceBar = {
+    type: "bar",
+    orientation: "h",
+    x: x_axis,
+    y: y_axis,
+    text: labels,
+    mode: "markers",
+    marker: {
+        color: colors
+    }            
+};
+
+
+
+// creatiing layout for bar chart
+let layout = {
+    title: {
+        text: "<b>top OTUs (up to 10) found in test subjects</b>",
+        font: {color: "black", size: 14}
+    },
+    width: 600,
+    height: 475
+};
+
+
+
+// plot the data into bar chart
+Plotly.newPlot("bar", [traceBar], layout)
+});
+};
+
+
+// function to build bubble chart
+function bubbleChart(sample) {
+
+
+
+// retrive json data
+d3.json(url).then((data)
