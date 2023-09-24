9f79929ddaf81b2f28bb051b53b45e41390c27ae
mmyrahim <mmyrahim@outlook.com>
2023-09-24 17:44:39 +0100

    error in code

---
+++ static/js/app.js
@@ -0,0 +1,72 @@
+// json url provided
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
+
+
+// retrive json data and log it
+d3.json(url).then(function(data) {
+    console.log(data);
+  });
+
+
+// initializing the dashboard at start up
+function init() {
+
+    
+
+// create drop down menu
+    let dropdownMenu = d3.select("#selDataset");
+
+    
+
+// get sample names to use in drop down menu and set as variable from the url provided
+    d3.json(url).then((data) => {
+        
+        let dataNames = data.names;
+        dataNames.forEach((id) => {
+            console.log(id);
+
+            dropdownMenu.append("option")
+            .text(id)
+            .property("value",id);
+        });
+
+        
+
+// get first sample from list and log it's ID
+        let testSubject = dataNames[0];
+
+        console.log(testSubject);
+
+        
+
+// build out the initial dashboard view for the first sample
+        demographicInfo(testSubject);
+        barChart(testSubject);
+        bubbleChart(testSubject);
+    });
+};
+
+
+// function to build demographic data fetches and displays the demographic metadata for a given sample
+function demographicInfo(sample) {
+
+    
+
+// retrive json data from the url
+    d3.json(url).then((data) => {
+
+        
+
+// set variables for demographic data and log and extract the metadata for the selected sample
+        let metadata = data.metadata;
+        let resultID = metadata.filter(result => result.id == sample);
+        console.log(resultID)
+
+        let firstID = resultID[0];
+
+        
+
+// clear out the existing metadata display
+        d3.select("#sample-metadata").html("");
+
+        
