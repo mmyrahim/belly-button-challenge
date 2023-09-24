76ec01be3f0615f525088504e7dd2821c6ea3fde
mmyrahim <mmyrahim@outlook.com>
2023-09-24 19:17:37 +0100

    adding code from activities, still facing error

--- static/js/app.js
+++ static/js/app.js
@@ -72,11 +72,11 @@ function demographicInfo(sample) {
         
 
 // add each key/value pair to the chart and log as they are being appended to the metadata display section
-Object.entries(firstID).forEach(([key,resultID]) => {
-    console.log(key,resultID);
-    d3.select("#sample-metadata").append("h5").text(`${key}: ${resultID}`);
-});
-});
+        Object.entries(firstID).forEach(([key,resultID]) => {
+            console.log(key,resultID);
+            d3.select("#sample-metadata").append("h5").text(`${key}: ${resultID}`);
+        });
+    });
 
 };
 
@@ -84,69 +84,132 @@ Object.entries(firstID).forEach(([key,resultID]) => {
 // function to build bar chart
 function barChart(sample) {
 
+    
 
+// retrive json data
+    d3.json(url).then((data) => {
 
-// retriveing json data
-d3.json(url).then((data) => {
         
 
 // set variables for bar chart and log
-let dataSamples = data.samples;
-let resultID = dataSamples.filter(result => result.id == sample);
-let firstID = resultID[0];
-let otu_ids = firstID.otu_ids;
-let otu_labels = firstID.otu_labels;
-let sample_values = firstID.sample_values;
+        let dataSamples = data.samples;
+        let resultID = dataSamples.filter(result => result.id == sample);
+        let firstID = resultID[0];
+        let otu_ids = firstID.otu_ids;
+        let otu_labels = firstID.otu_labels;
+        let sample_values = firstID.sample_values;
 
-console.log(otu_ids,otu_labels,sample_values);
+        console.log(otu_ids,otu_labels,sample_values);
 
-
+        
 
 // set up variables to graph (show in descending order)
-let y_axis = otu_ids.slice(0,10).map(id => `OTU ${id}`).reverse();
-let x_axis = sample_values.slice(0,10).reverse();
-let labels = otu_labels.slice(0,10).reverse();
-
-
+        let y_axis = otu_ids.slice(0,10).map(id => `OTU ${id}`).reverse();
+        let x_axis = sample_values.slice(0,10).reverse();
+        let labels = otu_labels.slice(0,10).reverse();
+        
+        
 
 // create trace for bar chart
-let colors = ['#FF33A1', '#FF5733', '#33FF57','#33FFA1', '#33A1FF', '#A1FF33', '#A133FF', '#A1FFA1', '#5733FF', '#FFA133',] 
-let traceBar = {
-    type: "bar",
-    orientation: "h",
-    x: x_axis,
-    y: y_axis,
-    text: labels,
-    mode: "markers",
-    marker: {
-        color: colors
-    }            
-};
+        let colors = ['#FF33A1', '#FF5733', '#33FF57','#33FFA1', '#33A1FF', '#A1FF33', '#A133FF', '#A1FFA1', '#5733FF', '#FFA133',] 
+        let traceBar = {
+            type: "bar",
+            orientation: "h",
+            x: x_axis,
+            y: y_axis,
+            text: labels,
+            mode: "markers",
+            marker: {
+                color: colors
+            }            
+        };
 
-
+        
 
 // creatiing layout for bar chart
-let layout = {
-    title: {
-        text: "<b>top OTUs (up to 10) found in test subjects</b>",
-        font: {color: "black", size: 14}
-    },
-    width: 600,
-    height: 475
-};
+        let layout = {
+            title: {
+                text: "<b>top OTUs (up to 10) found in test subjects</b>",
+                font: {color: "black", size: 14}
+            },
+            width: 600,
+            height: 475
+        };
 
-
+        
 
 // plot the data into bar chart
-Plotly.newPlot("bar", [traceBar], layout)
-});
+        Plotlyy.newPlot("bar", [traceBar], layout)
+    });
 };
 
 
 // function to build bubble chart
 function bubbleChart(sample) {
 
-
+    
 
 // retrive json data
-d3.json(url).then((data)
+    d3.json(url).then((data) => {
+        
+        
+
+// set variables for bubble chart and log
+        let dataSamples = data.samples
+        let resultID = dataSamples.filter(result => result.id == sample);
+        let firstID = resultID[0];
+
+        let otu_ids = firstID.otu_ids;
+        let otu_labels = firstID.otu_labels;
+        let sample_values = firstID.sample_values;
+
+        consolee.log(otu_ids,otu_labels,sample_values);
+        
+        
+
+// create trace for bubble chart
+        let traceBubble = {
+            x: otu_ids,
+            y: sample_values,
+            text: otu_labels,
+            mode: "markers",
+            marker: {
+                size: sample_values,
+                color: otu_ids,
+                colorscale: "Spectral-10"
+            }
+        };
+
+        
+
+// create layout for bubble chart
+        let layout = 
+            title: {
+                text: "<b>bacteria per sample</b>",
+                font: {color: "black", size: 22}
+            },
+            hovermode: "closest",
+            xaxis: {title: "OTU ID"},
+            yaxis: {title: "Sample Values"},
+            width: 1200, 
+            height: 500
+        };
+
+        
+
+// plot the data into bubble chart
+        Plotly.newPlot("bubble", [traceBubble], layout)
+    });
+};
+
+
+// function to update dashboard with each new selection, log, and call all functions
+function optionChanged(newID) { 
+    console.log(newID); 
+    demographicInfo(newID);
+    barChart(newID);
+    bubbleChart(newID);
+    
+};
+
+init();
